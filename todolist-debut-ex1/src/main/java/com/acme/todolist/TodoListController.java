efe618080cb513bf841dbc703bd79af945485272
vagrant <vagrant@debian12.localdomain>
2024-02-27 17:33:10 +0100

    Exercice 1 réalisé

--- todolist-debut-ex1/src/main/java/com/acme/todolist/TodoListController.java
+++ todolist-debut-ex1/src/main/java/com/acme/todolist/TodoListController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.inject.Inject;
+
 /**
  * Le controlleur Spring MVC qui expose les endpoints REST
  * 
@@ -24,6 +26,7 @@ public class TodoListController {
 	private static final String LATE = "[LATE!]";
 	private TodoItemRepository todoItemRepository;
 
+	@Inject
 	public TodoListController(TodoItemRepository todoItemRepository) {
 		super();
 		this.todoItemRepository = todoItemRepository;
@@ -37,7 +40,7 @@ public class TodoListController {
 	@ResponseStatus(code = HttpStatus.CREATED)
 	public void createTodoItem(@RequestBody TodoItem todoItem) {
 		// Code à compléter
-		// ...
+		todoItemRepository.save(todoItem);
 	}
 
 	@GetMapping("/todos")
